75c4e0e4daa5d776d3d1b753b66b7c006d634e93
Oussama BEN WAFI <oussamabenwafi@gmail.com>
2023-06-19 18:37:56 +0200

    test safe

--- src/main/java/com/example/pushSample/PushSampleApplication.java
+++ src/main/java/com/example/pushSample/PushSampleApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(MessageProperties.class)
 public class PushSampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PushSampleApplication.class, args);
