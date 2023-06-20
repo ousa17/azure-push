4d71bd38ae09b00e4081e835dfa381a97430598d
Oussama BEN WAFI <oussamabenwafi@gmail.com>
2023-06-20 10:55:36 +0200

    commit

--- src/main/java/com/example/pushSample/HelloController.java
+++ src/main/java/com/example/pushSample/HelloController.java
@@ -3,13 +3,16 @@ package com.example.pushSample;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.ExecutionException;
-
 @RestController
 public class HelloController {
+    private final MessageProperties properties;
+
+    public HelloController(MessageProperties properties) {
+        this.properties = properties;
+    }
 
     @GetMapping
-    public String getMessage() throws InterruptedException, ExecutionException {
-        return "Message: herer";
+    public String getMessage() {
+        return "Message: " + properties.getMessage();
     }
 }
